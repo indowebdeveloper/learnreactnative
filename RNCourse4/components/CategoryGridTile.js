46c61a181ebcfee0a5e161d534f4f8d96c237da0
Indowebdeveloper <git@indowebdeveloper.com>
2023-01-21 18:16:25 +0700

    oke

--- RNCourse4/components/CategoryGridTile.js
+++ RNCourse4/components/CategoryGridTile.js
@@ -1,10 +1,10 @@
-import { Pressable, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 const CategoryGridTile = ({ title, color }) => {
   return (
-    <View>
-      <Pressable>
-        <View>
+    <View style={styles.gridItem}>
+      <Pressable style={styles.button}>
+        <View style={styles.innerContainer}>
           <Text>{title}</Text>
         </View>
       </Pressable>
@@ -12,3 +12,30 @@ const CategoryGridTile = ({ title, color }) => {
   );
 };
 export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+  },
+  button: {
+    flex: 1,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
